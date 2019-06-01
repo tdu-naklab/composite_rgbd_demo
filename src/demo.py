f085d37ba2b41849898e1b2595c2975493224230
sota <shu.cyclone@gmail.com>
2019-06-01 17:49:32 +0900

    指定距離以上の画素を無視するようにした

--- src/demo.py
+++ src/demo.py
@@ -2,19 +2,26 @@ import pyrealsense2 as rs
 import numpy as np
 import cv2
 
+WIDTH = 640
+HEIGHT = 480
+FPS = 60
+THRESHOLD = 1.5  # これより遠い距離の画素を無視する
+
+
 def main():
     align = rs.align(rs.stream.color)
     config = rs.config()
-    config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
-    config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
+    config.enable_stream(rs.stream.color, WIDTH, HEIGHT, rs.format.bgr8, FPS)
+    config.enable_stream(rs.stream.depth, WIDTH, HEIGHT, rs.format.z16, FPS)
 
     pipeline = rs.pipeline()
     profile = pipeline.start(config)
-    depth_stream = profile.get_stream(rs.stream.color).as_video_stream_profile()
-    depth_intr = depth_stream.get_intrinsics()
+    depth_scale = profile.get_device().first_depth_sensor().get_depth_scale()
+    max_dist = THRESHOLD/depth_scale
 
     try:
         while True:
+            # フレーム取得
             frames = pipeline.wait_for_frames()
             aligned_frames = align.process(frames)
 
@@ -22,20 +29,32 @@ def main():
             depth_frame = aligned_frames.get_depth_frame()
             if not depth_frame or not color_frame:
                 continue
-        
-            color_image = np.asanyarray(color_frame.get_data())
+
+            # 深度画像
             depth_color_frame = rs.colorizer().colorize(depth_frame)
             depth_color_image = np.asanyarray(depth_color_frame.get_data())
 
-            images = np.hstack((color_image, depth_color_image))
+            # 指定距離以上を無視した深度画像
+            depth_image = np.asanyarray(depth_frame.get_data())
+            depth_filtered_image = (depth_image < max_dist) * depth_image
+            depth_gray_image = (depth_filtered_image * 255. / max_dist).reshape((HEIGHT, WIDTH)).astype(np.uint8)
+
+            # RGB画像
+            color_image = np.asanyarray(color_frame.get_data())
+
+            # 指定距離以上を無視したRGB画像
+            color_filtered_image = (depth_filtered_image.reshape((HEIGHT, WIDTH, 1)) > 0) * color_image
+
+            # 表示
             cv2.namedWindow('demo', cv2.WINDOW_AUTOSIZE)
-            cv2.imshow('demo', images)
+            cv2.imshow('demo', color_filtered_image)
             if cv2.waitKey(1) & 0xff == 27:
                 break
 
     finally:
         pipeline.stop()
         cv2.destroyAllWindows()
+
 
 if __name__ == "__main__":
     main()
