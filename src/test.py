77af665954360068243ddd3928155a98117144a9
sota <shu.cyclone@gmail.com>
2019-06-01 15:30:20 +0900

    RGBとDepthの画角を一致させる

--- src/test.py
+++ src/test.py
@@ -2,36 +2,40 @@ import pyrealsense2 as rs
 import numpy as np
 import cv2
 
-config = rs.config()
-config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
-config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
+def main():
+    align = rs.align(rs.stream.color)
+    config = rs.config()
+    config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
+    config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
 
-pipeline = rs.pipeline()
-profile = pipeline.start(config)
-depth_stream = profile.get_stream(rs.stream.color).as_video_stream_profile()
-depth_intr = depth_stream.get_intrinsics()
+    pipeline = rs.pipeline()
+    profile = pipeline.start(config)
+    depth_stream = profile.get_stream(rs.stream.color).as_video_stream_profile()
+    depth_intr = depth_stream.get_intrinsics()
 
-try:
-    while True:
-        # フレーム待ち(Color & Depth)
-        frames = pipeline.wait_for_frames()
-        color_frame = frames.get_color_frame()
-        depth_frame = frames.get_depth_frame()
-        if not depth_frame or not color_frame:
-            continue
-        color_image = np.asanyarray(color_frame.get_data())
-        # Depth画像
-        depth_color_frame = rs.colorizer().colorize(depth_frame)
-        depth_color_image = np.asanyarray(depth_color_frame.get_data())
+    try:
+        while True:
+            frames = pipeline.wait_for_frames()
+            aligned_frames = align.process(frames)
 
-        # 表示
-        images = np.hstack((color_image, depth_color_image))
-        cv2.namedWindow('RealSense', cv2.WINDOW_AUTOSIZE)
-        cv2.imshow('RealSense', images)
-        if cv2.waitKey(1) & 0xff == 27:
-            break
+            color_frame = aligned_frames.get_color_frame()
+            depth_frame = aligned_frames.get_depth_frame()
+            if not depth_frame or not color_frame:
+                continue
+        
+            color_image = np.asanyarray(color_frame.get_data())
+            depth_color_frame = rs.colorizer().colorize(depth_frame)
+            depth_color_image = np.asanyarray(depth_color_frame.get_data())
 
-finally:
-    # ストリーミング停止
-    pipeline.stop()
-    cv2.destroyAllWindows()
+            images = np.hstack((color_image, depth_color_image))
+            cv2.namedWindow('demo', cv2.WINDOW_AUTOSIZE)
+            cv2.imshow('demo', images)
+            if cv2.waitKey(1) & 0xff == 27:
+                break
+
+    finally:
+        pipeline.stop()
+        cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+    main()
